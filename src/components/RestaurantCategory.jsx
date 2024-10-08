7e1dbee0cec1c6ce82f4abed0dd2ad2006423a5b
Prabakaran VM <prabakaran12vm@gmail.com>
2024-10-08 15:34:00 +0530

    restaurantcategory comp added

---
+++ src/components/RestaurantCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+import { IoIosArrowUp,IoIosArrowDown   } from "react-icons/io";
+import { motion } from "framer-motion";
+
+const RestaurantCategory = ({ categoryData, showItems, setShowIndex }) => {
+  const { title, itemCards } = categoryData;
+  // logic to show and hide the accodion body
+  const showAccodionBody = (showItems) => {
+    setShowIndex();
+    // console.log('showItems', showItems);
+    // showItems = !showItems;
+  }
+  return (
+    
+    <div>
+      <div className="w-7/12 mx-auto  my-4 p-4 mt-6" >
+        {/* Accordion header */}
+        <div className="flex justify-between cursor-pointer" onClick={showAccodionBody}>
+          <span className="font-bold text-md">
+            {title} ({itemCards.length})
+          </span>
+          <span className="pr-5 ">{showItems ? <IoIosArrowUp size={20}/>  : <IoIosArrowDown size={20}/>}</span>
+        </div>
+        {/* Accordion body */}
+        {showItems && <ItemList itemCards={itemCards} />}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCategory;
