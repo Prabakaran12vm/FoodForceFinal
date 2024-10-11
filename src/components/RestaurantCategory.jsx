f8895dcdfd77b7535a25883bc436ccf73641395b
Prabakaran VM <prabakaran12vm@gmail.com>
2024-10-11 17:14:35 +0530

    restaurantcategory css added

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
-import { IoIosArrowUp,IoIosArrowDown   } from "react-icons/io";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import { motion } from "framer-motion";
 
-const RestaurantCategory = ({ categoryData, showItems, setShowIndex }) => {
+const RestaurantCategory = ({
+  categoryData,
+  showItems,
+  setShowIndex,
+  temp,
+}) => {
   const { title, itemCards } = categoryData;
   // logic to show and hide the accodion body
   const showAccodionBody = (showItems) => {
-    setShowIndex();
-    // console.log('showItems', showItems);
-    // showItems = !showItems;
-  }
+    setShowIndex(showItems);
+  };
   return (
-    
     <div>
-      <div className="w-7/12 mx-auto  my-4 p-4 mt-6" >
+      <div className="w-7/12 mx-auto my-4 p-4 mt-6 pl-[50px] ">
         {/* Accordion header */}
-        <div className="flex justify-between cursor-pointer" onClick={showAccodionBody}>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={showAccodionBody}
+        >
           <span className="font-bold text-md">
             {title} ({itemCards.length})
           </span>
-          <span className="pr-5 ">{showItems ? <IoIosArrowUp size={20}/>  : <IoIosArrowDown size={20}/>}</span>
+          <span className="pr-5 ">
+            {showItems ? (
+              <IoIosArrowUp size={20} />
+            ) : (
+              <IoIosArrowDown size={20} />
+            )}
+          </span>
         </div>
         {/* Accordion body */}
         {showItems && <ItemList itemCards={itemCards} />}
