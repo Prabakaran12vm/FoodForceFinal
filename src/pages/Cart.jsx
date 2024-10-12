9fef6677bddebb0429e276101f5c0bca170ed852
Prabakaran VM <prabakaran12vm@gmail.com>
2024-10-12 20:16:27 +0530

    cart  comp css added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "../components/ItemList";
 import { clearCart } from "../redux/slices/cartSlice";
-import { MdDelete } from "react-icons/md";
-import { BsCartX } from "react-icons/bs";
 
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 import { BsCartXFill } from "react-icons/bs";
+import CartShimmer from '../components/skeleton/CartShimmer'
+
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
@@ -15,33 +15,32 @@ const Cart = () => {
     // dispath an action
     dispatch(clearCart());
   };
+if (!cartItems) return <CartShimmer/>
+
+
   return (
-    <div className="text-center m-4 p-4">
-      <h1 className="text-2xl font-bold">Cart</h1>
+    <div className=" text-center m-4 p-4">
+      <h1 className="text-2xl font-bold mt-[30px]"></h1>
       <div>
         <div
           tabIndex={0}
           className="card absolute ml-[850px] card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-        >
-          <div className="card-body">
-            <span className="text-lg font-bold">8 Items</span>
-            <span className="text-info">Subtotal: $999</span>
-            <div className="card-actions">
-              <button className="btn bg-[#ff5200] hover:bg-[#ff3200] text-white btn-block">Checkout</button>
-            </div>
-          </div>
-        </div>
+        ></div>
       </div>
       <div className="w-6/12 m-auto">
         <ItemList itemCards={cartItems} />
         {/* cartItems.length === 0 */}
         {!cartItems.length ? (
-          <div className="mt-[100px]  ">
+          <div className="mt-[80px] h-[255px] items-center">
             {/* <h3>Cart is empty, Add items to the cart<h3/>  */}
-            <span className="">Cart is empty, Add items to the cart</span>
-            <span className="absolute pt-[20px] ">
-              <BsCartXFill size={50} />
+            <span className="text-xl">Cart is empty, Add items to the cart</span>
+            <div className=" "><span className=" flex ml-[270px] my-3  ">
+              <BsCartXFill size={60} color="ff5200" />
+              <span className="loading loading-ball loading-lg absolute ml-3  bg-[#ff5200] "></span>  
             </span>
+            
+            </div>
+            
           </div>
         ) : (
           <button
