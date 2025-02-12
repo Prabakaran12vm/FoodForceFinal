6c29cd0c21207daf36c834a977ff8c65c2ad036a
Prabakaran VM <prabakaran12vm@gmail.com>
2025-02-12 19:14:17 +0530

    api updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,8 @@ export const CDN_URL =
 export const LOGO_URL =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-yPxmHYHF8tgtmWhpC59Jk2PLpzbxaL-9bA&s';
 
-export const MENU_API ='https://food-api-o33x.onrender.com/api/restaurants'
+// export const MENU_API ='https://food-api-o33x.onrender.com/api/restaurants'
   // 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.89960&lng=80.22090&restaurantId=';
+export const MENU_API = 'http://foodapi.vercel.app/restaurants'
 export const DEFAULT_LATITUDE=12.89960
 export const DEFAULT_LONGITUDE=80.22090
